21e8008239b4d0ee1b8621ba62052ce1299644c4
G'olib <cleancode9294@gmail.com>
2023-04-22 22:43:05 +0500

    Create project vue js in laravel

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import PostComponent from "@/components/PostComponent.vue";
+app.component('post-component', PostComponent);
+
+
+
+app.mount('#app');
---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="m-5">
+        <create-component/>
+        <index-component/>
+    </div>
+
+<!--    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        </tbody>
+    </table>-->
+</template>
+
+<script>
+
+import createComponent from "@/components/CreateComponent.vue";
+import indexComponent from "@/components/IndexComponent.vue";
+
+export default {
+    name: "PostComponent",
+    components: {
+        createComponent,
+        indexComponent
+    },
+
+    data() {
+
+    },
+
+    computed: {},
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
